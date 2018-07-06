372f22ef3bf4cc20ea98836fbc9c2265b2bab519
Filipe Marins <filipe@goldman-marketing.com>
2018-07-06 20:42:03 -0300

    JS Boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,18 @@
+const ERROR = 2;
+const WARN = 1;
+
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "env": {
+      "es6": true
+    },
+    overrides: [
+    {
+      "files": [
+        "**/*.spec.js"
+      ],
+      "env": {
+        "jest": true
+      },
+    }]
 };
